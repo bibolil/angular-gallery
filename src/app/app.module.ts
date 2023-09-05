5dabe7217700adc8da861afca214b7b59bcb3d0a
Bilel Taboubi <bileltaboubi20@gmail.com>
2023-09-05 13:27:34 +0100

    last

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LightboxModule } from 'ngx-lightbox';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LightboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
